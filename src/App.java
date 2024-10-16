1f4a501f3e6b52a745873ced1ed4ff811678cd37
Maupau5 <mauroipaul@gmail.com>
2024-10-16 18:26:32 -0500

    Practica Binaria

--- src/App.java
+++ src/App.java
@@ -3,37 +3,68 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de personas: ");
+        int cantidadPersonas = sc.nextInt();
+        sc.nextLine();
 
-        MetodoBusqueda metodoBusqueda = new MetodoBusqueda();
-
-        ArrayList<Persona> listaPersonas = new ArrayList<>();  
-
-        System.out.print("Cantidad de personas que se dea ordenar: ");
-        int cantidadPersonas = scanner.nextInt();
-        scanner.nextLine(); 
+        Persona[] personas = new Persona[cantidadPersonas];
 
         for (int i = 0; i < cantidadPersonas; i++) {
-            System.out.println("\nPersona " + (i + 1) + ":");
+            System.out.println("\nIngrese datos de persona " + (i + 1) + ":");
             System.out.print("Nombre: ");
-            String nombre = scanner.nextLine();
-
+            String nombre = sc.nextLine();
             System.out.print("Edad: ");
-            int edad = scanner.nextInt();
-            scanner.nextLine();  
-
-            Persona persona = new Persona(nombre, edad);
-            listaPersonas.add(persona);
+            int edad = sc.nextInt();
+            sc.nextLine();
+            personas[i] = new Persona(nombre, edad);
         }
 
-        System.out.println("\nLista de personas ingresadas:");
-        for (Persona persona : listaPersonas) {
-            persona.mostrarDatos();
+        sortByEdad(personas);
+
+        System.out.println("Personas ordenadas por edad:");
+        System.out.println("\nPersonas ordenadas por edad:");
+        printArray(personas);
+
+        System.out.println("\nIngrese la edad a buscar:");
+        int edadBuscada = sc.nextInt();
+        int indice = findByEdad(personas, edadBuscada);
+        if (indice != -1) {
+            System.out.println("Persona encontrada: " + personas[indice]);
+            System.out.println("\nLa persona con la edad " + personas[indice]+" se llama: ");
+        } else {
+            System.out.println("No se encontró ninguna persona con la edad ingresada.");
         }
 
-
-
-        
+        sc.close();
     }
 
+    public static void sortByEdad(Persona[] personas) {
+        int n = personas.length;
+        for (int i = 1; i < n; i++) {
+            Persona actual = personas[i];
+            int j = i - 1;
+            while (j >= 0 && personas[j].getEdad() > actual.getEdad()) {
+                personas[j + 1] = personas[j];
+                j--;
+            }
+            personas[j + 1] = actual;
+        }
+    }
+
+    public static void printArray(Persona[] personas) {
+        for (Persona persona : personas) {
+            System.out.print("| " + persona + " |");
+        }
+        System.out.println();
+    }
+
+    public static int findByEdad(Persona[] personas, int edad) {
+        for (int i = 0; i < personas.length; i++) {
+            if (personas[i].getEdad() == edad) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,41 +1,33 @@
 import java.util.Scanner;
 
 public class Persona {
-    Scanner scanner = new Scanner(System.in);
-
     public String nombre;
     public int edad;
 
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = scanner.nextLine();
-    }
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        this.edad = scanner.nextInt();
-    }
-
-
-    public Persona() {
-
-    }
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
     }
-    public void mostrarDatos() {
-        System.out.println("Nombre: " + nombre + ", Edad: " + edad);
+
+    public String getNombre() {
+        return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
     @Override
     public String toString() {
         return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+        
     }
-    
 }
