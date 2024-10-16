20fe9f2b9a37021ec9cb61b2ddb81c53b9db7ae3
Maupau5 <mauroipaul@gmail.com>
2024-10-16 12:35:26 -0500

    Practica Binaria

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Persona> listaPersonas = new ArrayList<>();  
+
+        System.out.print("Cantidad de personas que se dea ordenar");
+        int cantidadPersonas = scanner.nextInt();
+        scanner.nextLine(); 
+
+        for (int i = 0; i < cantidadPersonas; i++) {
+            System.out.println("\nPersona " + (i + 1) + ":");
+            System.out.print("Nombre: ");
+            String nombre = scanner.nextLine();
+
+            System.out.print("Edad: ");
+            int edad = scanner.nextInt();
+            scanner.nextLine();  
+
+            Persona persona = new Persona(nombre, edad);
+            listaPersonas.add(persona);
+        }
+
+        System.out.println("\nLista de personas ingresadas:");
+        for (Persona persona : listaPersonas) {
+            persona.mostrarDatos();
+        }
+        
+
+        
     }
+
 }
---
+++ src/Persona.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Persona {
+    Scanner scanner = new Scanner(System.in);
+
+    public String nombre;
+    public int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = scanner.nextLine();
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = scanner.nextInt();
+    }
+
+
+    public Persona() {
+
+    }
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+    }
+    
+
+    
+
+}
